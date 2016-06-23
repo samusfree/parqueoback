ebef6d972a13efdf7bcaeec663970fcf9a321576
Samusfre <samusfree@gmail.com>
2016-06-22 23:50:52 -0500

    commit inicial

---
+++ parqueoback/src/main/java/com/hs/mostrito/ParqueobackApplication.java
@@ -0,0 +1,12 @@
+package com.hs.mostrito;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ParqueobackApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ParqueobackApplication.class, args);
+	}
+}
